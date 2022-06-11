831df4b58e84415a40e82455eeea5d3e9643ffbd
mikikralj5 <mikiprodan@gmail.com>
2022-06-11 17:20:36 +0200

    styling

---
+++ FoodDeliveryAPI/FoodDeliveryAPI/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDeliveryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DeliveryContext _context;
+
+        public AuthController(DeliveryContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        [Route("Register")]
+        //[Consumes("application/json")]
+        //[Produces("application/json")]
+        public async Task<ActionResult> RegisterUser([FromBody] User user)
+        {
+            if (UserExists(user.Username))
+            {
+
+                return BadRequest(new { mess = "vec postoji" });
+            }
+
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { mess = "napravljen" });
+
+        }
+
+        public IActionResult Login(string username, string password)
+        {
+            return Ok();
+        }
+
+
+        private bool UserExists(string id)
+        {
+            return _context.Users.Any(e => e.Username == id);
+        }
+    }
+}
--- FoodDeliveryAPI/FoodDeliveryAPI/Models/User.cs
+++ FoodDeliveryAPI/FoodDeliveryAPI/Models/User.cs
@@ -6,12 +6,12 @@ using Newtonsoft.Json.Converters;
 
 namespace FoodDeliveryAPI.Models
 {
-    //public enum UserType
-    //{
-    //    ADMIN,
-    //    CONSUMER,
-    //    DELIVERER
-    //}
+    public enum UserState
+    {
+        CONFIRMED,
+        DECLINED,
+        PENDING
+    }
 
     public class User
     {
@@ -20,7 +20,7 @@ namespace FoodDeliveryAPI.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string FirstName { get; set; }
+        public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Address { get; set; }
         public string Date { get; set; }
@@ -28,6 +28,7 @@ namespace FoodDeliveryAPI.Models
         public string UserType { get; set; }
         public byte[] Picture { get; set; }
         public List<Order> Orders { get; set; }
+        public UserState Verified { get; set; }
 
         public User()
         {
